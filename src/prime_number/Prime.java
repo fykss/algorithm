85f2a19e9ae9ef5e44bc9d2aac826e57d12f5bea
Влад Летун <fyks1649@gmail.com>
2019-03-10 19:17:42 +0200

    prime number

---
+++ src/prime_number/Prime.java
@@ -0,0 +1,26 @@
+package prime_number;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Range integer from 0 to n. Enter n: ");
+        int n = s.nextInt();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if ((i % j) == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) result.add(i);
+        }
+        System.out.println(result);
+    }
+}
